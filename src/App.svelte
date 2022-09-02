c6d539168e3933dc3c6e05a6f1cda289f2905c75
ZachGraceffa <zachgraceffa@gmail.com>
2022-09-01 21:11:05 -0500

    Added base template

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
 	export let resumeType
 	import ResumeTemplateZG from './components/ResumeZG.svelte'
+	import ResumeTemplateBase from './components/BaseResume.svelte'
 	import LetterPaper from './components/LetterPaper.svelte'
 	import type { Resume } from './types/Resume'
 	import resumeJson from '../public/resumes/general-back-end.json'
@@ -12,6 +13,8 @@
 	<LetterPaper>
 	{#if resumeType === 'ZG'}
 		<ResumeTemplateZG resumeData={resumeData} />
+	{:else if resumeType === 'Base'}
+		<ResumeTemplateBase resumeData={resumeData} />
 	{/if}
 	</LetterPaper>
 </main>
---
+++ src/components/BaseResume.svelte
@@ -0,0 +1,47 @@
+<script lang='ts'>
+    import type { Resume } from '../types/Resume'
+    export let resumeData: Resume
+</script>
+
+<article>
+    <header>
+        <h1>{resumeData.name}</h1>
+        <p>{resumeData.email}</p>
+        <p>{resumeData.phone}</p>
+        <p>{resumeData.website}</p>
+    </header>
+    <main>
+        {#if resumeData.jobs }
+        <h2>Work Experience</h2>
+        {/if}
+        {#each resumeData.jobs as job}
+        <div>
+            <h3>{job.title}</h3>
+            <span>{job.organization} - {job.location}</span>
+            <span>{job.duration}</span>
+            <p>{job.description}</p>
+        </div>
+        {/each}
+        {#if resumeData.education }
+        <h2>Education History</h2>
+        {/if}
+        {#each resumeData.education as institution}
+        <div>
+            <h3>{institution.name}</h3>
+            <span>{institution.location}</span>
+            <span>{institution.degree}</span>
+            <span>{institution.year}</span>
+        </div>
+        {/each}
+    </main>
+</article>
+
+<style>
+    article {
+        border: 1px solid black;
+    }
+
+    header > p {
+        margin: 0;
+    }
+</style>
